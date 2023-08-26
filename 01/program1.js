924a7ad7110a637d56e86d90443d71012ee3439d
ayush5718 <91721394+ayush5718@users.noreply.github.com>
2023-08-26 18:07:58 +0000

    saving the code

---
+++ 01/program1.js
@@ -0,0 +1,12 @@
+// primitive data type uses stack memory 
+// non primitive data types uses heap memory
+
+// const Name="    Aayush Kumar    "
+// const newName=Name.trim()  // it will remove the spaces 
+
+
+// const Name=new String("Aayush Kumar")  
+// console.log(Name.slice(3,-2));    // Aayush Kumar   Aay and ar will remove so ush Kum will print 
+
+// const url="www.ayushkumarblog.com"
+// console.log(url.replace("kumar","bhardwaj"))  // it will print www.ayushbhardwajblog.com 
