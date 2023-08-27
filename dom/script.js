3abd725e34ac1ba740569ad3fcb0c81e5d9baf16
ayush5718 <91721394+ayush5718@users.noreply.github.com>
2023-08-27 21:19:16 +0000

    chanfe

--- dom/script.js
+++ dom/script.js
@@ -7,12 +7,36 @@
 // document.querySelector('. classname, #id')  // single element selection 
 // document.querySelectorAll('h2') // multiple selection
 
-const parent = document.getElementById('parent')
+// const parent = document.getElementById('parent')
 
-const children= (parent.children)
+// const children= (parent.children)
 
-const arr = Array.from(children);
-arr.forEach((val)=>{
-          console.log(val.innerText)  // this will print all the inner text of the parent 
-})
- 
+// const arr = Array.from(children);
+// arr.forEach((val)=>{
+//         //  console.log(val.innerText)  // this will print all the inner text of the parent 
+// })
+//  console.log(parent.firstElementChild)
+
+// +++++++++++++++++++++++++++++++++++++++++ append child 
+
+// const element= document.createElement('div')  //creating a element
+
+// element.className="js-div" // adding a class name
+// element.innerText="This is div created by js" // adding inside text 
+// element.setAttribute("id","one")  // setting an id to the div
+// element.style.backgroundColor="red" // changing the bg-color
+// element.style.padding="15px" // adding padding to the div
+
+// document.body.appendChild(element)  // appending all the property change by the js
+
+// edit the html code by replacwith method
+
+// const child1=document.querySelector(".child-1")
+// child1.innerHTML="child-js"
+
+// const child2=document.createElement("div")
+// child2.className="child-2"
+// child2.innerHTML="This is child 1"
+// child1.replaceWith(child2)  // child1 will be replaces with child2
+
+// console.log(child1);
