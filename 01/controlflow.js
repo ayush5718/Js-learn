c88f854d056ea78c38e1e5f68f1c0da3fead6779
ayush5718 <91721394+ayush5718@users.noreply.github.com>
2023-08-27 12:46:02 +0000

    change

---
+++ 01/controlflow.js
@@ -0,0 +1,13 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++= nullish and coallish character
+
+//??- this character is basically made for firebase database sometime we get a null or undefined value and to ignore that type of pooblem we use this operator
+let val1= undefined??10  // 10 will be assigned to val1
+
+// console.log(val1);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++= ternary operator
+
+let val=100
+
+//   val<9? console.log("the number less than 9"):console.log("greater than 9");
+
